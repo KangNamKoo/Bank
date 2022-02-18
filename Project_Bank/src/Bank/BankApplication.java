48d46be8b1aa08f44858d86ac00aeb06ea8b594e
KangNamKoo <kill9989@naver.com>
2022-02-18 16:15:49 +0900

    Bank

--- Project_Bank/src/Bank/BankApplication.java
+++ Project_Bank/src/Bank/BankApplication.java
@@ -17,7 +17,7 @@ public class BankApplication {
 			System.out.println("--------------------------------------------");
 			System.out.println("1.계좌생성 | 2.계좌목록 | 3. 예금 | 4.출금 | 5.종료");
 			System.out.println("--------------------------------------------");
-			System.out.print("선택> "); 
+			System.out.print("선택> ");
 			
 			int selectNo = scanner.nextInt();
 			
